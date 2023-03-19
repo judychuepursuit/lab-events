288b1df8083e69cae3ecb037ad73fc14a1448cfe
Judy <judychue@pursuit.org>
2023-03-19 12:14:43 -0400

    lab-events passed

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+let colorButtons = document.querySelectorAll(".color")
+let currentColorSquare = document.querySelector("#current-color")
+
+for (const colorButton of colorButtons) {
+  colorButton.addEventListener("click", (event) => {
+    currentColorSquare.style.background = event.target.style.background
+  })
+}
+
+let colorCells = document.querySelectorAll(".cell")
+
+for (const cell of colorCells) {
+  cell.addEventListener("click", (event) => {
+    cell.style.background = currentColorSquare.style.background
+ })
+}
+
+
+
+
+
+
+
+
+//first part of the class
+//console.log(colorButtons)
+
+//let currentColor = document.querySelector("#current-color")
+//console.log(currentColor)
+
+//colorButtons[0].addEventListener("click", (event) => {
+  // console.log(event.target)
+  //console.log(event.target.style.background)
+  //event.target.style.background = "purple"
+
+  //currentColorSquare.style.background = "red"
+  // currentColorSquare.style.background = event.target.style.background
+//})
